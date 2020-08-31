6749362821b691ae73c321047e4d256119b3e953
chinmay6497 <36620836+chinmay6497@users.noreply.github.com>
2020-08-31 12:49:27 +0530

    Add files via upload

---
+++ advance_string.assingement.py
@@ -0,0 +1,12 @@
+#1) Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. If the string length is less than 2, return instead of the empty string. 
+# Go to the editor 
+# Sample String : 'w3resource' Expected Result : 'w3ce' 
+# Sample String : 'w3' Expected Result : 'w3w3' 
+# Sample String : ' w' Expected Result : Empty String
+https://github.com/chinmay6497/python_examples/blob/master/len_string.py
+
+# 2) Write a Python program to get a string from a given string where all occurrences of its first char have been changed to '$', except the first char itself
+https://github.com/chinmay6497/python_examples/blob/master/dollar.py
+
+# 3)Write a Python function to insert a string in the middle of a string.
+https://github.com/chinmay6497/python_examples/blob/master/string_middle.py
